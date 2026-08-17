b114b1638aaf1d11ea2df892cbbed4b26888c207
Anju Ajayakumar <anjuajay2468@gmail.com>
2026-08-17 14:58:36 +0530

    add three-node Raft election

--- internal/raft/server_test.go
+++ internal/raft/server_test.go
@@ -42,3 +42,64 @@ func TestRequestVoteHTTP(t *testing.T) {
 		)
 	}
 }
+func TestThreeNodeElection(t *testing.T) {
+	node1 := NewNode("node1")
+	node2 := NewNode("node2")
+	node3 := NewNode("node3")
+
+	node1.ClusterSize = 3
+	node2.ClusterSize = 3
+	node3.ClusterSize = 3
+
+	server2 := NewServer(node2)
+	server3 := NewServer(node3)
+
+	httpServer2 := httptest.NewServer(
+		http.HandlerFunc(server2.HandleRequestVote),
+	)
+	defer httpServer2.Close()
+
+	httpServer3 := httptest.NewServer(
+		http.HandlerFunc(server3.HandleRequestVote),
+	)
+	defer httpServer3.Close()
+
+	peers := []string{
+		httpServer2.URL,
+		httpServer3.URL,
+	}
+
+	elected := node1.StartElection(peers)
+
+	if !elected {
+		t.Fatal("node1 should have won the election")
+	}
+
+	if node1.State != Leader {
+		t.Fatalf(
+			"expected node1 to become leader, got %s",
+			node1.State,
+		)
+	}
+
+	if node1.CurrentTerm != 1 {
+		t.Fatalf(
+			"expected term 1, got %d",
+			node1.CurrentTerm,
+		)
+	}
+
+	if node2.VotedFor != "node1" {
+		t.Fatalf(
+			"expected node2 to vote for node1, got %s",
+			node2.VotedFor,
+		)
+	}
+
+	if node3.VotedFor != "node1" {
+		t.Fatalf(
+			"expected node3 to vote for node1, got %s",
+			node3.VotedFor,
+		)
+	}
+}
